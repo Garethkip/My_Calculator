f1ba98d37f064a498ac2827b1cd1de6120e4d107
Gareth Kipkoech <garethkip@gmail.com>
2026-09-18 12:38:58 +0300

    Implement local storage for Ans

--- myScript.js
+++ myScript.js
@@ -1,14 +1,21 @@
 const display = document.getElementById('display')
 const calculations = document.getElementById('calculations')
 const result = document.getElementById('resultDisplay')
-
+let answer = 0
+let prevAnswer = localStorage.getItem('prevAnswer')
 
 function calculate() { 
-
     console.log(calculations.textContent)
 
-    let answer = Math.round(eval(calculations.textContent) * 100) / 100
-
+    if (calculations.textContent.includes('Ans')) {
+        console.log('Ans found in calculations')
+        calculations.textContent = calculations.textContent.replace('Ans', prevAnswer)
+        answer = Math.round(eval(calculations.textContent) * 100)/100
+    }
+    else {
+        answer = Math.round(eval(calculations.textContent) * 100) / 100
+    }
+    
     // Use scientific notation for numbers >= 10^5
     if (Math.abs(answer) >= 100000) {
         answer = answer.toExponential(2)
@@ -17,13 +24,15 @@ function calculate() {
     result.textContent = '= ' + answer
 
     result.style.fontSize = '50px'
-    let prevAnswer = answer
+    // prevAnswer = answer
+    localStorage.setItem('prevAnswer', answer)
+    prevAnswer = localStorage.getItem('prevAnswer')
 }
 
 function Buttons(num){
     if (num === 'Ans') {
         console.log('Ans button clicked')
-        calculations.textContent += result.textContent.slice(2) // Append the previous answer without the '= ' prefix
+        calculations.textContent += 'Ans'
     } else {
         console.log(`${num} button clicked`)
         calculations.textContent += num
